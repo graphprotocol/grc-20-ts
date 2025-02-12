86fc0f671d12c8e690cc2a8589808f4864a8f46b
Byron Guina <byron.guina@gmail.com>
2025-02-12 12:01:01 -0800

    fix: correct expected value

--- src/core/base58.test.ts
+++ src/core/base58.test.ts
@@ -12,7 +12,7 @@ describe('base58', () => {
 
     // We check the length should be 22 in the ID.make() function and
     // re-run encodeBase58 if not.
-    expect(encoded.length === 22 || encoded.length === 21).toBe(22);
+    expect(encoded.length === 22 || encoded.length === 21).toBe(true);
 
     const decoded = decodeBase58ToUUID(encoded);
     expect(decoded).toHaveLength(expected.length);
