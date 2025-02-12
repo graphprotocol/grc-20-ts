3e176271b63b9981b923222ad1613d16df880c58
Byron Guina <byron.guina@gmail.com>
2025-02-12 10:54:52 -0800

    feat: only return ids with 22 characters
    In extremely rare occasions the id generator may result in ids that are 21 characters instead of 22. Theoretically the smallest length the id can generate is 16 characters, but only in specifically engineered cases.
    
    If this occurs we can generate again until we get a valid id.

--- src/id.ts
+++ src/id.ts
@@ -18,5 +18,16 @@ import { encodeBase58 } from './core/base58.js';
 export function make() {
   const uuid = uuidv4();
   const stripped = uuid.replaceAll(/-/g, '');
-  return encodeBase58(stripped);
+  const id = encodeBase58(stripped);
+
+  // In extremely rare occasions the id generator may result in ids that are
+  // 21 characters instead of 22. Theoretically the smallest length the id can
+  // generate is 16 characters, but only in specifically engineered cases.
+  //
+  // If this occurs we can generate again until we get a valid id.
+  if (id.length === 22) {
+    return id;
+  }
+
+  return make();
 }
