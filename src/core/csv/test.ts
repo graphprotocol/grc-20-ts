5e04ece5a71f4d65b7c69a736b6e644c207ff760
Byron Guina <byron.guina@gmail.com>
2025-03-04 09:31:49 -0800

    fix: test script

--- src/core/csv/test.ts
+++ src/core/csv/test.ts
@@ -1,7 +1,7 @@
 import { Ipfs } from '~/index.js';
-import { write } from './csv.js';
+import { stringify } from './csv.js';
 
-const stringified = write({
+const stringified = stringify({
   data: Array.from({ length: 151_000 }, (_, i: number) => [i.toString(), (i * 2).toString(), (i * 3).toString()]),
   metadata: {
     filetype: 'CSV',
@@ -22,5 +22,5 @@ const stringified = write({
   },
 });
 
-const cid = await Ipfs.uploadCSV(stringified.csv);
+const cid = await Ipfs.uploadCSV(stringified);
 console.log('cid', cid);
