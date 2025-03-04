a322cea431f8346e0fe145695604a62ca2c47bfd
Byron Guina <byron.guina@gmail.com>
2025-03-04 15:43:30 -0800

    feat: wrap calldata fetch with micro

--- src/encoding.ts
+++ src/encoding.ts
@@ -1,3 +1,9 @@
+import { Micro } from 'effect';
+
+class GetEditCalldataError extends Error {
+  readonly _tag = 'GetEditCalldataError';
+}
+
 type GetEditCalldataParams = {
   spaceId: string;
   cid: string;
@@ -5,14 +11,36 @@ type GetEditCalldataParams = {
 };
 
 export async function getEditCalldata(params: GetEditCalldataParams) {
-  const result = await fetch(`https://api-testnet.grc-20.thegraph.com/space/${params.spaceId}/edit/calldata`, {
-    method: 'POST',
-    body: JSON.stringify({
-      cid: params.cid,
-      // Optionally specify TESTNET or MAINNET. Defaults to MAINNET
-      network: params.network ?? 'MAINNET',
-    }),
+  const getCalldata = Micro.gen(function* () {
+    const result = yield* Micro.tryPromise({
+      try: () =>
+        fetch(`https://api-testnet.grc-20.thegraph.com/space/${params.spaceId}/edit/calldata`, {
+          method: 'POST',
+          body: JSON.stringify({
+            cid: params.cid,
+            // Optionally specify TESTNET or MAINNET. Defaults to MAINNET
+            network: params.network ?? 'MAINNET',
+          }),
+        }),
+      catch: error => new GetEditCalldataError(`Could not get edit calldata from space ${params.spaceId}: ${error}`),
+    });
+
+    const calldata = yield* Micro.tryPromise({
+      try: async () => {
+        const { to, data } = await result.json();
+        return {
+          to: to as `0x${string}`,
+          data: data as `0x${string}`,
+        };
+      },
+      catch: error =>
+        new GetEditCalldataError(
+          `Could not parse response from API when getting calldata for space ${params.spaceId}: ${error}`,
+        ),
+    });
+
+    return calldata;
   });
 
-  const { to, data } = await result.json();
+  return await Micro.runPromise(getCalldata);
 }
