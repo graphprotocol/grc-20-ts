8f409f5f7a6a0fdac7f3b699191e536e204da778
Nik Graf <nik@nikgraf.com>
2025-03-14 17:24:54 +0100

    feat: add deleteEntity

---
+++ .changeset/whole-years-trade.md
@@ -0,0 +1,5 @@
+---
+"@graphprotocol/grc-20": minor
+---
+
+add Graph.deleteEntity
--- src/graph/index.ts
+++ src/graph/index.ts
@@ -3,3 +3,4 @@ export * from './create-image.js';
 export * from './create-property.js';
 export * from './create-space.js';
 export * from './create-type.js';
+export * from './delete-entity.js';
