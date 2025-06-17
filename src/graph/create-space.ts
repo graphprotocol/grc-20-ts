5bf1ffa85ffc29c292dc8b6f0083f7adae7d2b9c
Byron Guina <byron.guina@gmail.com>
2025-06-17 09:54:09 -0700

    feat: optional ops + entity id for createSpace

--- src/graph/create-space.ts
+++ src/graph/create-space.ts
@@ -1,33 +1,45 @@
-import type { Op } from '../../proto.js';
-import { MAINNET_API_ORIGIN, TESTNET_API_ORIGIN } from './constants.js';
+import type {Op} from "../types.js"
+import {MAINNET_API_ORIGIN, TESTNET_API_ORIGIN} from "./constants.js"
 
 type Params = {
-  editorAddress: string;
-  name: string;
-  network?: 'TESTNET' | 'MAINNET';
-  ops?: Op[],
-  spaceEntityId?: string;
-};
+	editorAddress: string
+	name: string
+	network?: "TESTNET" | "MAINNET"
+	ops?: Op[]
+	spaceEntityId?: string
+}
 
 /**
  * Creates a space with the given name and editor address.
  */
 export const createSpace = async (params: Params) => {
-  const apiHost = params.network === 'TESTNET' ? TESTNET_API_ORIGIN : MAINNET_API_ORIGIN;
-  console.log('apiHost', apiHost);
-  const result = await fetch(`${apiHost}/deploy`, {
-    method: 'POST',
-    body: JSON.stringify({
-      spaceName: params.name,
-      initialEditorAddress: params.editorAddress,
-      ops: params.ops,
-      spaceEntity: params.spaceEntityId
-    }),
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
+	const apiHost = params.network === "TESTNET" ? TESTNET_API_ORIGIN : MAINNET_API_ORIGIN
+	console.log("apiHost", apiHost)
 
-  const jsonResult = await result.json();
-  return { id: jsonResult.spaceId };
-};
+	const formData = new FormData()
+	formData.append("name", params.name)
+	formData.append("editorAddress", params.editorAddress)
+	if (params.spaceEntityId) {
+		formData.append("spaceEntityId", params.spaceEntityId)
+	}
+
+	if (params.ops) {
+		formData.append("ops", JSON.stringify(params.ops))
+	}
+
+	const result = await fetch(`${apiHost}/deploy`, {
+		method: "POST",
+		body: JSON.stringify({
+			spaceName: params.name,
+			initialEditorAddress: params.editorAddress,
+			ops: params.ops,
+			spaceEntityId: params.spaceEntityId,
+		}),
+		headers: {
+			"Content-Type": "application/json",
+		},
+	})
+
+	const jsonResult = await result.json()
+	return {id: jsonResult.spaceId}
+}
