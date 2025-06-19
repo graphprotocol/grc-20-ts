94933d5afb6853cc75a95dae3f2bdecef9d4aa4b
Byron Guina <byron.guina@gmail.com>
2025-06-19 13:20:43 -0700

    feat: position sorting

--- src/core/position.ts
+++ src/core/position.ts
@@ -9,3 +9,32 @@ import { generateJitteredKeyBetween } from "fractional-indexing-jittered";
 export function generateBetween(first: string | null, second: string | null) {
   return generateJitteredKeyBetween(first, second);
 }
+
+/**
+ * A randomly generated position with jittering.
+ */
+export const DEFAULT = generateBetween(null, null);
+
+type MaybePosition = string | null
+
+function compare(a: MaybePosition, b: MaybePosition) {
+  // This ALWAYS puts nulls at the bottom, regardless of what 
+  // fractional indexes exist
+  if (a === null && b === null) return 0;
+  if (a === null) return 1;  // a goes to bottom
+  if (b === null) return -1; // b goes to bottom
+  return a.localeCompare(b);
+}
+
+/**
+ * Sorts a list of positions in ascending order. In
+ * the knowledge graph positions are optional. This
+ * function sorts null positions so they are always
+ * at the bottom.
+ * 
+ * @param positions - The list of positions to sort.
+ * @returns The sorted list of positions.
+ */
+export function sort(positions: MaybePosition[]): MaybePosition[] {
+  return positions.sort(compare);
+}
