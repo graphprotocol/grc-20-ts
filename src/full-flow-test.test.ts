2b0d67932571a4815ee8dc0f43c3eec2a4772289
Byron Guina <byron.guina@gmail.com>
2025-06-27 13:36:47 -0700

    feat: update testnet endpoint to prod url

--- src/full-flow-test.test.ts
+++ src/full-flow-test.test.ts
@@ -1,57 +1,60 @@
-import { privateKeyToAccount } from 'viem/accounts';
-import { it } from 'vitest';
-import { Ipfs } from '../index.js';
-import { createEntity } from './graph/create-entity.js';
-import { createSpace } from './graph/create-space.js';
-import { getWalletClient } from './smart-wallet.js';
+import { privateKeyToAccount } from "viem/accounts";
+import { it } from "vitest";
+import { Ipfs } from "../index.js";
+import { createEntity } from "./graph/create-entity.js";
+import { createSpace } from "./graph/create-space.js";
+import { getWalletClient } from "./smart-wallet.js";
 
-it.skip('should create a space', async () => {
-  const addressPrivateKey = '0xTODO';
+it.skip("should create a space", async () => {
+  const addressPrivateKey = "0xTODO";
   const { address } = privateKeyToAccount(addressPrivateKey);
   const smartAccountWalletClient = await getWalletClient({
     privateKey: addressPrivateKey,
   });
 
-  console.log('addressPrivateKey', addressPrivateKey);
-  console.log('address', address);
+  console.log("addressPrivateKey", addressPrivateKey);
+  console.log("address", address);
   // console.log('smartAccountWalletClient', smartAccountWalletClient);
 
   const space = await createSpace({
     editorAddress: address,
-    name: 'test (nik2)',
-    network: 'TESTNET',
+    name: "test (nik2)",
+    network: "TESTNET",
   });
 
-  console.log('space', space);
+  console.log("space", space);
   const spaceId = space.id;
 
   const { ops, id } = await createEntity({
-    name: 'test (nik2)',
-    description: 'test (nik2)',
+    name: "test (nik2)",
+    description: "test (nik2)",
   });
-  console.log('entity id', id);
+  console.log("entity id", id);
 
   const { cid } = await Ipfs.publishEdit({
-    name: 'Edit name',
+    name: "Edit name",
     ops,
     author: address,
   });
 
-  console.log('cid', cid);
+  console.log("cid", cid);
 
-  const result = await fetch(`https://hypergraph-v2-testnet.up.railway.app/space/${spaceId}/edit/calldata`, {
-    method: 'POST',
-    body: JSON.stringify({ cid }),
-  });
+  const result = await fetch(
+    `https://api-testnet.geobrowser.io/space/${spaceId}/edit/calldata`,
+    {
+      method: "POST",
+      body: JSON.stringify({ cid }),
+    }
+  );
 
-  console.log('edit result', result);
+  console.log("edit result", result);
 
   const editResultJson = await result.json();
-  console.log('editResultJson', editResultJson);
+  console.log("editResultJson", editResultJson);
   const { to, data } = editResultJson;
 
-  console.log('to', to);
-  console.log('data', data);
+  console.log("to", to);
+  console.log("data", data);
 
   const txResult = await smartAccountWalletClient.sendTransaction({
     // @ts-expect-error - TODO: fix the types error
@@ -61,5 +64,5 @@ it.skip('should create a space', async () => {
     data: data,
   });
 
-  console.log('txResult', txResult);
+  console.log("txResult", txResult);
 }, 30000);
